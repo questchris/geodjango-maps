e969f5bb291a849eba678b60987e053b84af4248
questchris <chris@questbridge.org>
2014-11-11 21:35:57 -0800

    Started high school model

--- mapping/schools/models.py
+++ mapping/schools/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class HighSchool(models.Model):
+    name = models.CharField(max_length=255)
+    fips = models.CharField('FIPS Code',
+                            max_length=5,
+                            unique=True,
+                            primary_key=True)
+    point = models.PointField()
+    lon = models.FloatField('longitude')
+    lat = models.FloatField('latitude')
+
+    objects = models.GeoManager()
+
+    def __str__(self):
+        return self.name
